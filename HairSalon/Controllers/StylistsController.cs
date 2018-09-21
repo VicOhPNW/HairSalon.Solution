6f91c6d8b7da6c8e46a192267fea4154fb50f4d8
Victoria Oh <ohvictori@gmail.com>
2018-09-21 14:39:13 -0700

    Build and route to Details.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -11,5 +11,31 @@ namespace HairSalon.Controllers
     {
       return View(Stylist.GetAll());
     }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string stylistName)
+    {
+      Stylist newStylist = new Stylist(stylistName);
+      newStylist.Save();
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
+
+    [HttpGet("/stylist/{id}")]
+    public ActionResult Details(int id)
+    {
+      // Dictionary<string, object> model = new Dictionary<string, object>();
+      // Stylist selectedStylist = Stylist.Find(id);
+      // List<Client> StylistClients = selectedStylist.GetClients();
+      // model.Add("stylist", selectedStylist);
+      // model.Add("clients", StylistClients);
+      return View("Details");
+    }
   }
 }
