d385c76284a041dcdeaff71a1b28a8d7bb3513de
Victoria Oh <ohvictori@gmail.com>
2018-09-21 13:53:37 -0700

    Add Controller files. Confirm Home Index routes to Stylist Index.

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      return View(Stylist.GetAll());
+    }
+  }
+}
